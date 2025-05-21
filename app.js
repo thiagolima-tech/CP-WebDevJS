a6744436fd914ce43ffdbeafc9f56908b547f333
Thiago Lima <thiagolima.age@gmail.com>
2025-05-21 10:30:54 -0300

    fix: html e nomes

--- app.js
+++ app.js
@@ -1,74 +1,76 @@
-products = [{
-    nome: 'mouse',
+products = [
+  {
+    nome: "mouse",
     preco: 20,
-    categoria: 'perifericos',
-    disponibilidade: true
-},
-{
-    nome: 'teclado',
+    categoria: "perifericos",
+    disponibilidade: true,
+  },
+  {
+    nome: "teclado",
     preco: 110,
-    categoria: 'perifericos',
-    disponibilidade: true
-},
-{
-    nome: 'fone',
+    categoria: "perifericos",
+    disponibilidade: true,
+  },
+  {
+    nome: "fone",
     preco: 50,
-    categoria: 'perifericos',
-    disponibilidade: false
-},
-{
-    nome: 'salame',
+    categoria: "perifericos",
+    disponibilidade: false,
+  },
+  {
+    nome: "salame",
     preco: 10,
-    categoria: 'alimentos',
-    disponibilidade: true
-},
-{
-    nome: 'pipoca',
+    categoria: "alimentos",
+    disponibilidade: true,
+  },
+  {
+    nome: "pipoca",
     preco: 5,
-    categoria: 'alimentos',
-    disponibilidade: false
-},
-{
-    nome: 'tomate',
+    categoria: "alimentos",
+    disponibilidade: false,
+  },
+  {
+    nome: "tomate",
     preco: 4,
-    categoria: 'alimentos',
-    disponibilidade: true
-},
-{
-    nome: 'macarrao',
+    categoria: "alimentos",
+    disponibilidade: true,
+  },
+  {
+    nome: "macarrao",
     preco: 10,
-    categoria: 'alimentos',
-    disponibilidade: false
-},
-{
-    nome: 'camisa',
+    categoria: "alimentos",
+    disponibilidade: false,
+  },
+  {
+    nome: "camisa",
     preco: 70,
-    categoria: 'roupas',
-    disponibilidade: true
-},
-{
-    nome: 'calca',
+    categoria: "roupas",
+    disponibilidade: true,
+  },
+  {
+    nome: "calca",
     preco: 100,
-    categoria: 'roupas',
-    disponibilidade: true
-},
-{
-    nome: 'tenis',
+    categoria: "roupas",
+    disponibilidade: true,
+  },
+  {
+    nome: "tenis",
     preco: 30,
-    categoria: 'roupas',
-    disponibilidade: true
-}]
+    categoria: "roupas",
+    disponibilidade: true,
+  },
+];
 
+const categorias = document.getElementById("categorias");
+const areaProducts = document.getElementById("produtos");
+const btnFiltro = document.getElementById("btnFiltrar");
+const btnTodos = document.getElementById("btnTodos");
+const checkDisponiveis = document.getElementById("checkbox");
 
-
-const filtro = document.getElementById('filtro')
-const areaProducts = document.getElementById('produtos')
-const btnClicar = document.getElementById('btnClick')
-
-filtro.addEventListener('mouseover', () => {
-    let areaProduct = document.createElement('div')
-    areaProduct.innerHTML = `
+filtro.addEventListener("mouseover", () => {
+  let areaProduct = document.createElement("div");
+  areaProduct.innerHTML = `
     <h1> Texto </h1>
-    `
-    products.append(product)
-})
+    `;
+  products.append(product);
+});
